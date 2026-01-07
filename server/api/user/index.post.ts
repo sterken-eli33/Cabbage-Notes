4668c90ddf6e7f6f2c6d53f3017e1af09c1772fe
Eli Sterken <sterk.eli@outlook.com>
2026-01-07 13:05:44 -0800

    fix - Error When User Already Exists Respond with an error if a user tries to create an account using an existing email.

--- server/api/user/index.post.ts
+++ server/api/user/index.post.ts
@@ -1,7 +1,18 @@
+import { eq } from 'drizzle-orm';
 import { hashSync, genSaltSync } from 'bcrypt-ts';
 
 export default eventHandler(async event => {
     const body = await readValidatedBody(event, createUserSchema.parse);
+
+    const existingUser = await db.select().from(schema.users).where(eq(schema.users.email, body.email));
+
+    if(existingUser.length > 0) {
+        throw createError({
+            status: 400,
+            statusMessage: "It looks like a user with that email address already exists!"
+        });
+    };
+
     const result = await db.insert(schema.users).values({
         email: body.email,
         password: hashSync(body.password, genSaltSync())
